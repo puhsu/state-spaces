158070f87199480d46077effd259194b7041f28e
Maksim Nakhodnov <nakhodnov17@gmail.com>
2022-11-07 14:15:28 +0300

    Add S4Model

---
+++ model/s4_model.py
@@ -0,0 +1,83 @@
+import torch.nn as nn
+
+from .s4d import S4D
+
+
+class S4Model(nn.Module):
+
+    def __init__(
+        self,
+        d_input,
+        d_output=10,
+        d_model=256,
+        n_layers=4,
+        dropout=0.2,
+        prenorm=False,
+        block_class=S4D,
+        block_kwargs=None,
+        dropout_fn=nn.Dropout1d
+    ):
+        super().__init__()
+
+        self.prenorm = prenorm
+
+        # Linear encoder (d_input = 1 for grayscale and 3 for RGB)
+        self.encoder = nn.Linear(d_input, d_model)
+
+        # Stack S4 layers as residual blocks
+        self.s4_layers = nn.ModuleList()
+        self.norms = nn.ModuleList()
+        self.dropouts = nn.ModuleList()
+
+        if block_kwargs is None:
+            block_kwargs = {}
+        block_kwargs['lr'] = min(block_kwargs.get('lr', 0.001), 0.001)
+        block_kwargs['transposed'] = block_kwargs.get('transposed', True)
+        block_kwargs['dropout'] = block_kwargs.get('dropout', dropout)
+        for _ in range(n_layers):
+            self.s4_layers.append(
+                block_class(d_model, **block_kwargs)
+            )
+            self.norms.append(nn.LayerNorm(d_model))
+            self.dropouts.append(dropout_fn(dropout))
+
+        # Linear decoder
+        self.decoder = nn.Linear(d_model, d_output)
+
+    def forward(self, x):
+        """
+        Input x is shape (B, L, d_input)
+        """
+        x = self.encoder(x)  # (B, L, d_input) -> (B, L, d_model)
+
+        x = x.transpose(-1, -2)  # (B, L, d_model) -> (B, d_model, L)
+        for layer, norm, dropout in zip(self.s4_layers, self.norms, self.dropouts):
+            # Each iteration of this loop will map (B, d_model, L) -> (B, d_model, L)
+
+            z = x
+            if self.prenorm:
+                # Prenorm
+                z = norm(z.transpose(-1, -2)).transpose(-1, -2)
+
+            # Apply S4 block: we ignore the state input and output
+            z, _ = layer(z)
+
+            # Dropout on the output of the S4 block
+            z = dropout(z)
+
+            # Residual connection
+            x = z + x
+
+            if not self.prenorm:
+                # Postnorm
+                x = norm(x.transpose(-1, -2)).transpose(-1, -2)
+
+        x = x.transpose(-1, -2)
+
+        # Pooling: average pooling over the sequence length
+        x = x.mean(dim=1)
+
+        # Decode the outputs
+        x = self.decoder(x)  # (B, d_model) -> (B, d_output)
+
+        return x
