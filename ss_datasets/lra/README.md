0355c4b89b4708beecfb879738d13fe3a9c83608
viktoroo <viktoroo.sch@gmail.com>
2022-11-08 17:05:33 +0300

    lra configuration docs

--- ss_datasets/lra/README.md
+++ ss_datasets/lra/README.md
@@ -1,2 +1,3 @@
 * Download dataset: `sh download.sh`
 * Target dataset files are located at `lra_release/lra_release/pathfinder128/` -- pass this as `data_dir` to loaders.
+* Ready to use configuration is available in `configure.py`
--- ss_datasets/lra/configure.py
+++ ss_datasets/lra/configure.py
@@ -2,6 +2,19 @@ from ss_datasets.lra.loader import PathFinder
 
 
 def configure_lra() -> PathFinder:
+    """Usage example:
+    >>> from ss_datasets.lra.configure import configure_lra
+    >>> pf = configure_lra()
+    >>> iterator = iter(pf.train_dataloader())
+    >>> next(iterator)
+    (tensor([[[-1.],
+         [-1.],
+         [-1.],
+         ...,
+         [-1.],
+         [-1.],
+         [-1.]]]), tensor([0]), {'rate': 1})
+    """
     pf = PathFinder(_name_='pathfinder', data_dir='ss_datasets/lra/lra_release/lra_release/pathfinder128/')
     pf.setup()
     return pf
