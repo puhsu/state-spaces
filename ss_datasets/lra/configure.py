fca855ae9418bb4f3eb01885e23a1ae072d83264
viktoroo <viktoroo.sch@gmail.com>
2022-11-14 17:14:08 +0300

    added X configuration in PathX

--- ss_datasets/lra/configure.py
+++ ss_datasets/lra/configure.py
@@ -1,8 +1,10 @@
 from ss_datasets.lra.loader import PathFinder
 
 
-def configure_lra() -> PathFinder:
-    """Usage example:
+def configure_lra(x: int = 128) -> PathFinder:
+    """
+    :param x: 64, 128 or 256
+    Usage example:
     >>> from ss_datasets.lra.configure import configure_lra
     >>> pf = configure_lra()
     >>> iterator = iter(pf.train_dataloader())
@@ -15,6 +17,6 @@ def configure_lra() -> PathFinder:
          [-1.],
          [-1.]]]), tensor([0]), {'rate': 1})
     """
-    pf = PathFinder(_name_='pathfinder', data_dir='ss_datasets/lra/lra_release/lra_release/pathfinder128/')
+    pf = PathFinder(_name_='pathfinder', data_dir=f'ss_datasets/lra/lra_release/lra_release/pathfinder{x}/')
     pf.setup()
     return pf
